ce8b0a6f0358a3f2a08a808361b885ce493e0e5d
Nandarizqiamelia <nandarizqiameliaa@gmail.com>
2024-05-07 16:20:05 +0700

    desti-lavarel

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        h1 {
+            text-align: center ;
+          color: rgb(14, 79, 207);
+          font-size: 16px;
+        }
+
+       p {
+        text-align: center ;
+          color: rgb(14, 79, 207);
+          font-size: 16px;
+        }
+      </style>
+</head>
+<body>
+    <h1>SELAMAT DATANG!</h1>
+    <p>Terima kasih telah bergabung di website kami. Media belajar kita bersama!</p>
+</body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>SELAMAT DATANG GYSS!</h1>
+    <p>Terima kasih telah bergabung di website kami. Media belajar kita bersama ><..!</p>
+    <?php echo $_GET["first-name"]; ?> 
+    <?php echo $_GET["last-name"]; ?><br>
+    <?php echo $_GET["gender"]; ?><br>
+    <?php echo $_GET["nationality"]; ?><br>
+    <?php echo $_GET["language"]; ?><br>
+    <?php echo $_GET["bio"]; ?><br>
+</body>
+</html>
